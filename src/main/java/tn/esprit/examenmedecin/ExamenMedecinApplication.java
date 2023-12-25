b38c5b8a6a2267dc998c49964dc7d94af630cbaa
chayma-attafi <chayma.attafi@esprit.tn>
2023-12-25 20:26:26 +0100

    first commit

---
+++ src/main/java/tn/esprit/examenmedecin/ExamenMedecinApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.examenmedecin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMedecinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMedecinApplication.class, args);
+	}
+
+}
